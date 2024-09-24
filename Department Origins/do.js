2509328315cc820349212afb8a2919a9ea80067f
Max-Lovell <max_lovell@hotmail.co.uk>
2024-09-24 16:15:50 +0100

    Reverse geocode using overpass

--- Department Origins/do.js
+++ Department Origins/do.js
@@ -14,7 +14,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const popup = L.popup();
 
 function onMapClick(e) {
-    //getLocationDetails(e.latlng)
+    overpassTurboQuery(e.latlng)
     popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
@@ -30,138 +30,31 @@ function saveClick(latlng){
 }
 map.on('click', onMapClick);
 
-async function getLocationDetails(latLng){
-    url = 'api.geonames.org/countrySubdivisionJSON?lat='+latLng.lat+'&lng='+latLng.lng+'&maxRows=10&radius=40&username=demo'
-    // Make the fetch request with the provided options
-    fetch(url)
-    .then(response => {
-        // Check if the request was successful
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
-        }
-        // Parse the response as JSON
-        return response.json();
-    })
-    .then(data => {
-        // Handle the JSON data
-        console.log(data);
-    })
-    .catch(error => {
-        // Handle any errors that occurred during the fetch
-        console.error('Fetch error:', error);
-    });
-}
-
-
-
-// BOUNDARIES **************************************************
-// Add interactivity to country layer
-function onEachBoundary(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-// Event handlers for highlighting countries
-function highlightFeature(e) {
-    const layer = e.target
-    console.log(layer.feature.properties)
-    layer.setStyle({
-        weight: 2,
-        color: 'blue',
-        fillOpacity: 0.2
-    })
-    .bringToFront()
-    .bindPopup(layer.feature.properties.ADMIN);
-}
-
-function resetHighlight(e) {
-    console.log(e)
-    countryLayer.resetStyle(e.target);
-}
-
-// Zoom to a feature on click
-function zoomToFeature(e) {
-    console.log(e.target.feature.properties)
-    map.fitBounds(e.target.getBounds());
-    // Example usage: Fetch boundaries for "Germany" with a maximum admin level of 8
-    fetchAdminBoundaries(e.target.feature.properties.ISO_A2);
-}
-
-function fetchAdminBoundaries(iso) {
-    // Construct the Overpass API query
+function overpassTurboQuery(latLng){
     const query = `
         [out:json];
-        relation["ISO3166-2"~"^${iso}"]
-        ["admin_level"="6"]
-        ["type"="boundary"]
-        ["boundary"="administrative"];
+        (
+        is_in(${latLng.lat},${latLng.lng})->.a;
+        area.a[admin_level][boundary=administrative];
+        );
         out body;
         >;
         out skel qt;
-    `;
-
+    `
     // Encode the query for the URL
     const url = "https://overpass-api.de/api/interpreter?data=" + encodeURIComponent(query);
-
     // Fetch the JSON data from the Overpass API
     fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log("Administrative boundaries data:", data);
-        // Convert the Overpass JSON to GeoJSON using osmtogeojson
-        const geojson = osmtogeojson(data);
-        // Plot the GeoJSON on the Leaflet map
-        const newLayer = L.geoJson(geojson, layerParams)
-        newLayer.addTo(map);
-        // Fit the map bounds to the GeoJSON layer
-        map.fitBounds(L.geoJson(geojson).getBounds());
+        for(let d=0; d<data.elements.length; d++){
+            const tags = data.elements[d].tags
+            console.log(tags.int_name ? tags.int_name : 'name:en' in tags ? tags['name:en'] : tags.name)
+        }
     })
     .catch(error => {
         console.error("Error fetching administrative boundaries:", error);
     });
 }
-
-// Load GeoJSON data for countries
-const layerParams = {
-    style: {
-        fillColor: 'blue',
-        weight: 1,
-        opacity: 1,
-        color: 'black',
-        fillOpacity: 0
-    },
-    onEachFeature: onEachBoundary
-}
-
-const countryLayer = L.geoJson.ajax("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson", layerParams);
-
-// Add country layer by default (initial zoom level)
-countryLayer.addTo(map);
-
-// // Switch layers based on zoom level
-// map.on('zoomend', function () {
-//     var currentZoom = map.getZoom();
-
-//     // Set a threshold zoom level (e.g., 6)
-//     if (currentZoom >= 6) {
-//         // Remove country layer and add city layer
-//         if (map.hasLayer(countryLayer)) {
-//             map.removeLayer(countryLayer);
-//         }
-//         if (!map.hasLayer(cityLayer)) {
-//             map.addLayer(cityLayer);
-//         }
-//     } else {
-//         // Remove city layer and add country layer
-//         if (map.hasLayer(cityLayer)) {
-//             map.removeLayer(cityLayer);
-//         }
-//         if (!map.hasLayer(countryLayer)) {
-//             map.addLayer(countryLayer);
-//         }
-//     }
-// });
 
