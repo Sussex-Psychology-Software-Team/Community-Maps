dc502c7c17ca40c8d6720a2aa4f62a0b8cfe22ad
Max-Lovell <max_lovell@hotmail.co.uk>
2024-09-24 16:42:45 +0100

    Simple version

--- Department Origins/do.js
+++ Department Origins/do.js
@@ -1,4 +1,5 @@
 const userData = {lat:0,lng:0}
+let marker = null;
 
 // Create map
 const map = L.map('map',{
@@ -7,19 +8,13 @@ const map = L.map('map',{
     .setView([30, 0], 3);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    //maxZoom: 10,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const popup = L.popup();
-
 function onMapClick(e) {
-    overpassTurboQuery(e.latlng)
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-    const marker = L.marker(e.latlng).addTo(map);
+    if(marker !== null) marker.remove() 
+    marker = L.marker(e.latlng).addTo(map);
     saveClick(e.latlng)
 }
 
@@ -29,33 +24,3 @@ function saveClick(latlng){
     console.log(userData)
 }
 map.on('click', onMapClick);
-
-function overpassTurboQuery(latLng){
-    const query = `
-        [out:json];
-        (
-        is_in(${latLng.lat},${latLng.lng})->.a;
-        area.a[admin_level][boundary=administrative];
-        );
-        out body;
-        >;
-        out skel qt;
-    `
-    // Encode the query for the URL
-    const url = "https://overpass-api.de/api/interpreter?data=" + encodeURIComponent(query);
-    // Fetch the JSON data from the Overpass API
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log("Administrative boundaries data:", data);
-        for(let d=0; d<data.elements.length; d++){
-            const tags = data.elements[d].tags
-            if(!tags) return
-            console.log('int_name' in tags ? tags.int_name : 'name:en' in tags ? tags['name:en'] : tags.name)
-        }
-    })
-    .catch(error => {
-        console.error("Error fetching administrative boundaries:", error);
-    });
-}
-
