ec5b8ce1c820c07436d4b6163dd3db3c71bd6dcc
Max-Lovell <max_lovell@hotmail.co.uk>
2024-09-25 12:07:36 +0100

    Simplify positioning and buttons

--- Department Origins/Qualtrics/qdo.js
+++ Department Origins/Qualtrics/qdo.js
@@ -1,41 +1,33 @@
 Qualtrics.SurveyEngine.addOnload(function()
 {
-	/*Place your JavaScript here to run when the page loads*/
-	const userData = {lat:0,lng:0}
-    let marker = null
-
+    const userData = {lat:0,lng:0}
+    let marker = null;
+    
     // Create map
     const map = L.map('map',{
             //zoomSnap: 0.5
         })
-        .setView([30, 0], 3)
-
+        .setView([30, 0], 3);
+    
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         //maxZoom: 10,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map)
-
-
-    const popup = L.popup()
-
-    map.on('click', onMapClick)
+    }).addTo(map);
+    
     function onMapClick(e) {
         if(marker !== null) marker.remove() 
         marker = L.marker(e.latlng).addTo(map);
         saveClick(e.latlng)
     }
-
-    // Qualtrics stuff --------------
+    
     function saveClick(latlng){
         userData.lat = latlng.lat
         userData.lng = latlng.lng
         Qualtrics.SurveyEngine.setEmbeddedData('latitude', userData.lat )
         Qualtrics.SurveyEngine.setEmbeddedData('longitude', userData.lng )
+        console.log(userData)
     }
-
-    document.getElementById('mapsNextButton').addEventListener('click',()=>{
-            document.getElementById('NextButton').click()
-        })
+    map.on('click', onMapClick);
 });
 
 Qualtrics.SurveyEngine.addOnReady(function()
