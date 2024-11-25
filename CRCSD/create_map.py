583a91ba28e49f1b34103b9a82d0dcdadb4e7fd3
max-lovell <max_lovell@hotmail.co.uk>
2024-11-25 10:39:32 +0000

    Delete API Key

--- CRCSD/create_map.py
+++ CRCSD/create_map.py
@@ -1,6 +1,6 @@
 from geocoder import geolocate
 from map import createMap
 
-API_KEY = "AIzaSyCAIQUuKwQOAAxNZQvXqLLtuDN75lfyeIA"
+API_KEY = ""
 df = geolocate('CRCSD/CRCSD_Institutions.csv', 'CRCSD/CRCSD_Institutions.csv', API_KEY)
 createMap('CRCSD/CRCSD_Institutions.csv', 'docs/CRCSD/index.html')
